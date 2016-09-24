b96f3be9b6fe20255a821f675ad36fea8093e094
Lahari Kommi <lkommi@ncsu.edu>
2016-09-23 22:57:26 -0400

    added admin column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160922225324) do
+ActiveRecord::Schema.define(version: 20160924025206) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email"
@@ -23,9 +23,10 @@ ActiveRecord::Schema.define(version: 20160922225324) do
   create_table "members", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
     t.index ["email"], name: "index_members_on_email", unique: true
   end
 
